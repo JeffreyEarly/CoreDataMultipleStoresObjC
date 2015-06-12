46c34f9203b083c5ec7530a1ca129902497992cc
Jeffrey J. Early <jeffrey@jeffreyearly.com>
2015-06-12 08:38:11 -0700

    Initial Commit

---
+++ CoreDataMultipleStoreObjC/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  CoreDataMultipleStoreObjC
+//
+//  Created by Jeffrey Early on 6/12/15.
+//  Copyright © 2015 Jeffrey Early. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+
+
+@end
+
---
+++ CoreDataMultipleStoreObjC/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  CoreDataMultipleStoreObjC
+//
+//  Created by Jeffrey Early on 6/12/15.
+//  Copyright © 2015 Jeffrey Early. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
